bcc027e27e26d7f1894ee51eea04ee2a9eafaedf
naabir <miralamgir422@gmail.com>
2024-09-25 06:04:31 +0600

    added some part

--- js/link.js
+++ js/link.js
@@ -17,3 +17,16 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     })
 })
+
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    
+    
+    if (window.scrollY > 50) {  
+        navbar.classList.add('backdrop-blur-md', 'bg-opacity-90');  
+    } else {
+        navbar.classList.remove('backdrop-blur-md', 'bg-opacity-90'); 
+    }
+});
+
